136ff9a0ccc8471227443ef77c8a061b1732c9a3
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-04-11 11:38:34 -0400

    Updated footer and navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,7 +7,7 @@
         </div>
         <div class="hidden sm:block">
             <a href="#services" class="text-sm font-normal tracking-wider mx-4 hover:text-indigo-600">{{__("Services")}}</a>
-            <a href="" class="text-sm font-normal tracking-wider mx-4 hover:text-indigo-600">{{__("Podcast")}}</a>
+            <a href="https://voicebits.co/shows/JQo8hZKo" target="_blank" class="text-sm font-normal tracking-wider mx-4 hover:text-indigo-600">{{__("Podcast")}}</a>
             <a href="#contact" class="text-sm font-normal tracking-wider mx-4 hover:text-indigo-600">{{__("Contact")}}</a>
         </div>
 
